45f5a5cfa8493d9fb32ea00a83529a27664748dc
pandiji <skm808478@gmail.com>
2022-12-27 15:54:18 +0530

    firstnew

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function opt_func()
 {
     b.classList.add("slideleft");
     console.log("slided left")
+    b.classList.remove("slideright");
 }
 cross.onclick = function(){cross_func()};
 function cross_func()
 {
     b.classList.add("slideright");
     console.log("slided right")
+    b.classList.remove("slideleft");
 }
